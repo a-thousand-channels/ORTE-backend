3d8a62e1542bef0115284f4f546dca39740c0c2b
ut <ulf.treger@googlemail.com>
2018-05-12 17:38:06 +0100

    adding admin group views and controller

---
+++ app/controllers/admin/groups_controller.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+class Admin::GroupsController < ApplicationController
+  before_action :set_admin_group, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @admin_groups = Group.all
+  end
+
+  def show
+  end
+
+  def new
+    @admin_group = Group.new
+  end
+
+  def edit
+  end
+
+  def create
+    @admin_group = Group.new(admin_group_params)
+
+    respond_to do |format|
+      if @admin_group.save
+        format.html { redirect_to  [:admin, @admin_group], notice: 'Group was successfully created.' }
+        format.json { render :show, status: :created, location: @admin_group }
+      else
+        format.html { render :new }
+        format.json { render json: @admin_group.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /admin_groups/1
+  # PATCH/PUT /admin_groups/1.json
+  def update
+    respond_to do |format|
+      if @admin_group.update(admin_group_params)
+        format.html { redirect_to  [:admin, @admin_group], notice: 'Group was successfully updated.' }
+        format.json { render :show, status: :ok, location: @admin_group }
+      else
+        format.html { render :edit }
+        format.json { render json: @admin_group.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /admin_groups/1
+  # DELETE /admin_groups/1.json
+  def destroy
+    @admin_group.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_groups_url, notice: 'Group was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_admin_group
+      @admin_group = Group.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def admin_group_params
+      params.require(:admin_group).permit(:title, :user_id)
+    end
+end
