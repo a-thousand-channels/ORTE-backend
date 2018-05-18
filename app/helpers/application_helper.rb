92d36e6dc135a070fdba795e558511e8a101750e
ut <ulf.treger@googlemail.com>
2018-05-18 19:24:29 +0100

    UI: places form. places index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,17 +12,20 @@ module ApplicationHelper
   end
 
   def smart_date_display(startdate,enddate)
-    if !enddate
-      "#{I18n.l(startdate, format: '%A')[0..1]} #{startdate.strftime("%d.%m.%y, %H:%M")}"
-    elsif startdate > enddate
-      "#{I18n.l(startdate, format: '%A')[0..1]} #{startdate.strftime("%d.%m.%y, %H:%M")}"
-    elsif  startdate == enddate
-      "#{I18n.l(startdate, format: '%A')[0..1]} #{startdate.strftime("%d.%m.%y, %H:%M")}"
-    elsif startdate.to_date == enddate.to_date
-      "#{I18n.l(startdate, format: '%A')[0..1]} #{startdate.strftime("%d.%m.%y, %H:%M")} ‒ #{enddate.strftime("%H:%M")}"
-    else
-      "#{I18n.l(startdate, format: '%A')[0..1]} #{startdate.strftime("%d.%m.%y, %H:%M")} ‒ #{I18n.l(enddate, format: '%A')[0..1]} #{enddate.strftime("%d.%m.%y, %H:%M")}"
+    if !startdate
+      '?'
+      return
     end
-
+    if !enddate
+      "#{startdate.strftime("%d.%m.%y, %H:%M")}"
+    elsif startdate > enddate
+      "#{startdate.strftime("%d.%m.%y, %H:%M")}"
+    elsif  startdate == enddate
+      "#{startdate.strftime("%d.%m.%y, %H:%M")}"
+    elsif startdate.to_date == enddate.to_date
+      "#{startdate.strftime("%d.%m.%y, %H:%M")} ‒ #{enddate.strftime("%H:%M")}"
+    else
+      "#{startdate.strftime("%d.%m.%y, %H:%M")} ‒ #{enddate.strftime("%d.%m.%y, %H:%M")}"
+    end
   end
 end
