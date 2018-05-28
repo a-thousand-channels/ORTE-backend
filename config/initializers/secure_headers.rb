771914f143ab3ead2476bc2e2cf08900ccd4baeb
ut <ulf.treger@googlemail.com>
2018-05-28 19:01:10 +0100

    deployment prepare/2

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -42,7 +42,7 @@ SecureHeaders::Configuration.default do |config|
     report_uri: %w(/report_csp)
   }
 
-  if Rails.env.development? || Rails.env.test?
+  if Rails.env.development? || Rails.env.test? || Rails.env.staging?
     # get rid off the https
     config.csp = default_csp_config.merge({
         default_src: %w('self'),
