1ee53800118363ec58f67f6190dbb060e6f0f321
ut <ulf.treger@googlemail.com>
2018-05-29 17:58:09 +0100

    1 fix

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -94,7 +94,7 @@ class PlacesController < ApplicationController
   def destroy
     @place.destroy
     respond_to do |format|
-      format.html { redirect_to map_layer_places_path([@map,@layer]), notice: 'Place was successfully destroyed.' }
+      format.html { redirect_to map_layer_places_path(@map,@layer), notice: 'Place was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
