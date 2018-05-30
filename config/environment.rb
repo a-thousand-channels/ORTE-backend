329bad0e422103793c98939115cd83ac1ca82851
ut <ulf.treger@googlemail.com>
2018-05-30 13:09:06 +0100

    example smtp settings

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,20 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+
+ActionMailer::Base.delivery_method = :smtp
+
+ActionMailer::Base.smtp_settings = {
+
+      enable_starttls_auto: true,
+      address: "domain.org",
+      port: '465',
+      ssl: true,
+      domain: "domain.org",
+      authentication: :login,
+      user_name: "sender@domain.org",
+      password: "abcdefgh"
+
+}
