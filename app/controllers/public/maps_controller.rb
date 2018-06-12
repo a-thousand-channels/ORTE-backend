c4c2976c3825c68cea1fcb5a4045c7e89481da80
ut <ulf.treger@googlemail.com>
2018-06-12 19:19:24 +0100

    set cors header

--- app/controllers/public/maps_controller.rb
+++ app/controllers/public/maps_controller.rb
@@ -2,6 +2,19 @@
 
 class Public::MapsController  < ActionController::Base
 
+  before_filter :cors_set_access_control_headers
+
+  # For all responses in this controller, return the CORS access control headers.
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'GET'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  end
+
+
+
   # GET /maps.json
   def index
     @maps = Map.published
