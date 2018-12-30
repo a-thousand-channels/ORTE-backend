b7e8037ef0969e35dbd5aaec4ab5ab733377fb79
ut <ulf.treger@googlemail.com>
2018-12-30 20:10:40 +0100

    fixing routing default rspecs

--- spec/routing/layers_routing_spec.rb
+++ spec/routing/layers_routing_spec.rb
@@ -4,35 +4,35 @@ RSpec.describe LayersController, type: :routing do
   describe "routing" do
 
     it "routes to #index" do
-      expect(:get => "/layers").to route_to("layers#index")
+      expect(:get => "/maps/1/layers").to route_to("layers#index", map_id: '1')
     end
 
     it "routes to #new" do
-      expect(:get => "/layers/new").to route_to("layers#new")
+      expect(:get => "/maps/1/layers/new").to route_to("layers#new", map_id: '1')
     end
 
     it "routes to #show" do
-      expect(:get => "/layers/1").to route_to("layers#show", :id => "1")
+      expect(:get => "/maps/1/layers/1").to route_to("layers#show", :id => "1", map_id: '1')
     end
 
     it "routes to #edit" do
-      expect(:get => "/layers/1/edit").to route_to("layers#edit", :id => "1")
+      expect(:get => "/maps/1/layers/1/edit").to route_to("layers#edit", :id => "1", map_id: '1')
     end
 
     it "routes to #create" do
-      expect(:post => "/layers").to route_to("layers#create")
+      expect(:post => "/maps/1/layers").to route_to("layers#create", map_id: '1')
     end
 
     it "routes to #update via PUT" do
-      expect(:put => "/layers/1").to route_to("layers#update", :id => "1")
+      expect(:put => "/maps/1/layers/1").to route_to("layers#update", :id => "1", map_id: '1')
     end
 
     it "routes to #update via PATCH" do
-      expect(:patch => "/layers/1").to route_to("layers#update", :id => "1")
+      expect(:patch => "/maps/1/layers/1").to route_to("layers#update", :id => "1", map_id: '1')
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/layers/1").to route_to("layers#destroy", :id => "1")
+      expect(:delete => "/maps/1/layers/1").to route_to("layers#destroy", :id => "1", map_id: '1')
     end
 
   end
