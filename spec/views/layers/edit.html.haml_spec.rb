18487123ef341548fa809579bc961b6109867909
ut <ulf.treger@googlemail.com>
2018-12-30 20:39:56 +0100

    rspec views layers

--- spec/views/layers/edit.html.haml_spec.rb
+++ spec/views/layers/edit.html.haml_spec.rb
@@ -2,26 +2,28 @@ require 'rails_helper'
 
 RSpec.describe "layers/edit", type: :view do
   before(:each) do
+    group = FactoryBot.create(:group)
+    user = FactoryBot.create(:admin_user, :group_id => group.id)
+    sign_in user
+    @map = FactoryBot.create(:map, :group_id => group.id)
     @layer = assign(:layer, Layer.create!(
       :title => "MyString",
       :text => "MyString",
       :published => false,
-      :map => nil
+      :map => @map
     ))
   end
 
   it "renders the edit layer form" do
     render
 
-    assert_select "form[action=?][method=?]", layer_path(@layer), "post" do
+    assert_select "form[action=?][method=?]", map_layer_path(@layer, :map_id => @map.id), "post" do
 
       assert_select "input[name=?]", "layer[title]"
 
       assert_select "input[name=?]", "layer[text]"
 
-      assert_select "input[name=?]", "layer[published]"
 
-      assert_select "input[name=?]", "layer[map_id]"
     end
   end
 end
--- spec/views/layers/new.html.haml_spec.rb
+++ spec/views/layers/new.html.haml_spec.rb
@@ -2,26 +2,25 @@ require 'rails_helper'
 
 RSpec.describe "layers/new", type: :view do
   before(:each) do
+    group = FactoryBot.create(:group)
+    user = FactoryBot.create(:admin_user, :group_id => group.id)
+    sign_in user
+    @map = FactoryBot.create(:map, :group_id => group.id)
     assign(:layer, Layer.new(
       :title => "MyString",
       :text => "MyString",
       :published => false,
-      :map => nil
+      :map => @map
     ))
+
   end
 
   it "renders new layer form" do
     render
 
-    assert_select "form[action=?][method=?]", layers_path, "post" do
+    assert_select "form[action=?][method=?]", map_layers_path(@map.id), "post" do
 
       assert_select "input[name=?]", "layer[title]"
-
-      assert_select "input[name=?]", "layer[text]"
-
-      assert_select "input[name=?]", "layer[published]"
-
-      assert_select "input[name=?]", "layer[map_id]"
     end
   end
 end
