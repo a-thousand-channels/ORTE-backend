cbab4020affa55cce082a7dbbeb6484aa51348db
ut <ulf.treger@googlemail.com>
2018-12-30 20:10:40 +0100

    fixing routing default rspecs

--- spec/routing/icons_routing_spec.rb
+++ spec/routing/icons_routing_spec.rb
@@ -4,35 +4,35 @@ RSpec.describe IconsController, type: :routing do
   describe "routing" do
 
     it "routes to #index" do
-      expect(:get => "/icons").to route_to("icons#index")
+      expect(:get => "/iconsets/1/icons").to route_to("icons#index", iconset_id: '1')
     end
 
     it "routes to #new" do
-      expect(:get => "/icons/new").to route_to("icons#new")
+      expect(:get => "/iconsets/1/icons/new").to route_to("icons#new", iconset_id: '1')
     end
 
     it "routes to #show" do
-      expect(:get => "/icons/1").to route_to("icons#show", :id => "1")
+      expect(:get => "/iconsets/1/icons/1").to route_to("icons#show", :id => "1", iconset_id: '1')
     end
 
     it "routes to #edit" do
-      expect(:get => "/icons/1/edit").to route_to("icons#edit", :id => "1")
+      expect(:get => "/iconsets/1/icons/1/edit").to route_to("icons#edit", :id => "1", iconset_id: '1')
     end
 
     it "routes to #create" do
-      expect(:post => "/icons").to route_to("icons#create")
+      expect(:post => "/iconsets/1/icons").to route_to("icons#create", iconset_id: '1')
     end
 
     it "routes to #update via PUT" do
-      expect(:put => "/icons/1").to route_to("icons#update", :id => "1")
+      expect(:put => "/iconsets/1/icons/1").to route_to("icons#update", :id => "1", iconset_id: '1')
     end
 
     it "routes to #update via PATCH" do
-      expect(:patch => "/icons/1").to route_to("icons#update", :id => "1")
+      expect(:patch => "/iconsets/1/icons/1").to route_to("icons#update", :id => "1", iconset_id: '1')
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/icons/1").to route_to("icons#destroy", :id => "1")
+      expect(:delete => "/iconsets/1/icons/1").to route_to("icons#destroy", :id => "1", iconset_id: '1')
     end
 
   end
--- spec/routing/places_routing_spec.rb
+++ spec/routing/places_routing_spec.rb
@@ -4,35 +4,35 @@ RSpec.describe PlacesController, type: :routing do
   describe "routing" do
 
     it "routes to #index" do
-      expect(:get => "/places").to route_to("places#index")
+      expect(:get => "/maps/1/layers/1/places").to route_to("places#index", layer_id: '1', map_id: '1')
     end
 
     it "routes to #new" do
-      expect(:get => "/places/new").to route_to("places#new")
+      expect(:get => "/maps/1/layers/1/places/new").to route_to("places#new", layer_id: '1', map_id: '1')
     end
 
     it "routes to #show" do
-      expect(:get => "/places/1").to route_to("places#show", :id => "1")
+      expect(:get => "/maps/1/layers/1/places/1").to route_to("places#show", :id => "1", layer_id: '1', map_id: '1')
     end
 
     it "routes to #edit" do
-      expect(:get => "/places/1/edit").to route_to("places#edit", :id => "1")
+      expect(:get => "/maps/1/layers/1/places/1/edit").to route_to("places#edit", :id => "1", layer_id: '1', map_id: '1')
     end
 
     it "routes to #create" do
-      expect(:post => "/places").to route_to("places#create")
+      expect(:post => "/maps/1/layers/1/places").to route_to("places#create", layer_id: '1', map_id: '1')
     end
 
     it "routes to #update via PUT" do
-      expect(:put => "/places/1").to route_to("places#update", :id => "1")
+      expect(:put => "/maps/1/layers/1/places/1").to route_to("places#update", :id => "1", layer_id: '1', map_id: '1')
     end
 
     it "routes to #update via PATCH" do
-      expect(:patch => "/places/1").to route_to("places#update", :id => "1")
+      expect(:patch => "/maps/1/layers/1/places/1").to route_to("places#update", :id => "1", layer_id: '1', map_id: '1')
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/places/1").to route_to("places#destroy", :id => "1")
+      expect(:delete => "/maps/1/layers/1/places/1").to route_to("places#destroy", :id => "1", layer_id: '1', map_id: '1')
     end
 
   end
