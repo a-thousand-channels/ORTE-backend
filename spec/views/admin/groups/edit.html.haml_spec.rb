ff920520e09dbf0c663e35424a4db0fabc53b5ae
ut <ulf.treger@googlemail.com>
2018-12-31 16:42:41 +0100

    rspec views groups

--- spec/views/admin/groups/edit.html.haml_spec.rb
+++ spec/views/admin/groups/edit.html.haml_spec.rb
@@ -1,21 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe "groups/edit", type: :view do
+RSpec.describe "admin/groups/edit", type: :view do
   before(:each) do
-    @group = assign(:group, Group.create!(
-      :title => "MyString",
-      :user => nil
+    @admin_group = assign(:group, Group.create!(
+      :title => "MyString"
     ))
+    user = FactoryBot.create(:admin_user, :group_id => @admin_group.id)
   end
 
   it "renders the edit group form" do
     render
 
-    assert_select "form[action=?][method=?]", group_path(@group), "post" do
+    assert_select "form[action=?][method=?]", admin_group_path(@admin_group), "post" do
 
-      assert_select "input[name=?]", "group[title]"
+      assert_select "input[name=?]", "admin_group[title]"
 
-      assert_select "input[name=?]", "group[user_id]"
     end
   end
 end
