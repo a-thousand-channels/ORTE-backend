77c82078a9af52bc231f5f35d903c3ed884737a8
ut <ulf.treger@googlemail.com>
2018-12-31 16:51:48 +0100

    rspecs

--- spec/views/admin/users/index.html.haml_spec.rb
+++ spec/views/admin/users/index.html.haml_spec.rb
@@ -4,9 +4,10 @@ require 'rails_helper'
 
 RSpec.describe 'admin/users/index', type: :view do
   before(:each) do
+    group = FactoryBot.create(:group)
     assign(:admin_users, Kaminari.paginate_array([
-                                                   FactoryBot.create(:user),
-                                                   FactoryBot.create(:user)
+                                                   FactoryBot.create(:user, :group_id => group.id),
+                                                   FactoryBot.create(:user, :group_id => group.id)
                                                  ]).page(1))
   end
 
