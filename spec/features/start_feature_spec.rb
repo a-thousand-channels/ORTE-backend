80215b46b8a66b4007b928f80504b7decac93d8c
ut <ulf.treger@googlemail.com>
2019-01-15 14:56:14 +0100

    js/capybara testing, login feature

--- spec/features/start_feature_spec.rb
+++ spec/features/start_feature_spec.rb
@@ -2,23 +2,35 @@
 
 require 'rails_helper'
 
-describe 'some stuff which requires js', js: true do
-  it 'will use the specific js driver (webkit headless)', driver: :chrome do
+
+RSpec.feature 'Visitors view startpage and ' do
+
+  background do
+    # ...
+  end
+
+  scenario 'will use the specific js driver', :js => true do
     visit root_path
-    save_and_open_page
     expect(page).to have_content('Javascript enabled')
   end
+
 end
 
-describe 'Guests', js: false do
-  xit 'shows login form' do
-    visit '/'
+RSpec.feature  'Login' do
+
+  background do
+    # ...
+  end
+  scenario 'shows login form', :js => true do
+    visit root_path
+    click_link 'Sign in'
     expect(page).to have_current_path '/users/sign_in'
-    expect(page).to have_content 'You need to sign in'
+    expect(page).to have_content 'Sign in'
   end
 
-  xit 'shows error if login with wrong credentials' do
-    visit '/'
+  scenario 'shows error if login with wrong credentials' do
+    visit root_path
+    click_link 'Sign in'
     fill_in 'user_email', with: 'nobody@nowhere.com'
     fill_in 'user_password', with: 'abcdefghijklmn'
     click_button 'Log in'
