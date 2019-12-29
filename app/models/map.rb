4aacbb532e070882a9603579a93ead1219596fa9
ut <ulf.treger@googlemail.com>
2019-12-29 16:10:47 +0100

    Some UI tweaks for navbar, map views, footer

--- app/models/map.rb
+++ app/models/map.rb
@@ -2,6 +2,8 @@ class Map < ApplicationRecord
   belongs_to :group
   has_many :layers
 
+  validates :title,  presence: true
+
   # call me: Map.by_user(current_user).find(params[:id])
   scope :by_user, lambda { |user|
     where(:group_id => user.group.id) unless user.group.title == 'Admins'
