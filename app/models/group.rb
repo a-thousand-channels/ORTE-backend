cbeab82f1ec83a10f60d22a12395ffd3bc6a12ed
ut <ulf.treger@googlemail.com>
2019-12-29 18:39:36 +0100

    rspecs

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,8 @@ class Group < ApplicationRecord
   has_many :users
   has_many :maps
 
+  validates :title,  presence: true
+
   # call me: Group.by_user(current_user).find(params[:id])
   scope :by_user, lambda { |user|
     where(:id => user.group.id) unless user.group.title == 'Admins'
