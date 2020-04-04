c132c00c7206569c3e1786bf96073ff1456b2d97
ut <ulf.treger@googlemail.com>
2020-04-04 18:40:17 +0200

    adding custom webfont to asset pipeline, finally

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,13 +6,21 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
+
+Rails.application.config.assets.paths << Rails.root.join('vendor/assets/img')
+Rails.application.config.assets.paths << Rails.root.join('vendor/assets/fonts')
+
+# Rails.application.config.assets.paths << Rails.root.join('vendor','assets','fonts','media','files')
+Rails.application.config.assets.paths << Rails.root.join('vendor','assets','fonts','worksans')
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-Rails.application.config.assets.paths << Rails.root.join('vendor')
-Rails.application.config.assets.paths << Rails.root.join('vendor/img')
-Rails.application.config.assets.paths << Rails.root.join('app/assets/stylesheets/fonts/worksans')
+
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
+# sprockets 3.x syntax:
+# Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+# sprockets 4.x syntax:
+Rails.application.config.assets.precompile << ["*.svg", "*.eot", "*.woff", "*.woff2", "*.ttf"]
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( leaflet-color-markers.js )
