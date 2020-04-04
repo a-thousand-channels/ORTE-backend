2d087484d0721ce4759efbb1ff42f9222659a934
ut <ulf.treger@googlemail.com>
2020-04-04 18:40:17 +0200

    adding custom webfont to asset pipeline, finally

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,8 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
+
+// Images and fonts so that views can link to them
+//
+//= link_tree ../../../vendor/assets/stylesheets/
+//= link_tree ../../../vendor/assets/fonts/
