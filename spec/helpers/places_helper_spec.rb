672f06dc0a20c80b20096ad20f54ed57ce4fda7b
ut <ulf.treger@googlemail.com>
2020-04-19 20:16:34 +0200

    [UI] Small UI changes for Re-Map button etc

--- spec/helpers/places_helper_spec.rb
+++ spec/helpers/places_helper_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe PlacesHelper, type: :helper do
 
   describe "edit_link" do
     it 'it returns a visual edit link' do
-      expect(helper.edit_link(1,2,3)).to eq(" <a href=\"/maps/1/layers/2/places/3/edit\"><i class='fi fi-pencil'></i></a>")
+      expect(helper.edit_link(1,2,3)).to eq(" <a href=\"/maps/1/layers/2/places/3/edit\" class='edit_link'><i class='fi fi-pencil'></i></a>")
     end
   end
 
