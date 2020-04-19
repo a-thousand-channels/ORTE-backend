f92300e6746bc857913c26341813bafd8956ce94
ut <ulf.treger@googlemail.com>
2020-04-19 20:17:12 +0200

    [UI] connect new image upload to place edit form

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -13,6 +13,10 @@ module PlacesHelper
     #html_concat link_to(edit_map_layer_place_url(map_id, layer_id, id)) {
     #  html_tag :i, 'aria-hidden': 'true', class: 'fa fa-pencil'
     #}
-    " <a href=\"/maps/#{map_id}/layers/#{layer_id}/places/#{id}/edit\"><i class='fi fi-pencil'></i></a>"
+    " <a href=\"/maps/#{map_id}/layers/#{layer_id}/places/#{id}/edit\" class='edit_link'><i class='fi fi-pencil'></i></a>"
+  end
+
+  def add_new_entry_link(place)
+      "/maps/#{place.layer.map.id}/layers/#{place.layer.id}/places/new?location=#{@place.location}&address=#{@place.address}&zip=#{@place.zip}&city=#{@place.city}&lat=#{@place.lat}&lon=#{@place.lon}";
   end
 end
