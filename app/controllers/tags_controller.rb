7b8ed38e0da84e5692bc9ebc52a76e37bd8366aa
ut <ulf.treger@googlemail.com>
2020-08-26 20:00:03 +0200

    [Feature} Show list of all tags per map, show places for one tag/2

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class TagsController < ApplicationController
+  def index
+    @map = Map.by_user(current_user).find(params[:map_id])
+    @tags = @map.places.all_tags
+  end
+
+  def show
+    @map = Map.by_user(current_user).find(params[:map_id])
+    @tag =  ActsAsTaggableOn::Tag.find(params[:id])
+    @places = Place.tagged_with(@tag.name)
+  end
+end
