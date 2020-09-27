e720b0d771dcc4056a10c5eb04da9e9db7ef2a04
ut <ulf.treger@googlemail.com>
2020-09-27 13:13:41 +0200

    Adding leaflet icon settings for iconsets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_25_164945) do
+ActiveRecord::Schema.define(version: 2020_09_27_103213) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -52,6 +52,10 @@ ActiveRecord::Schema.define(version: 2020_09_25_164945) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "icon_anchor"
+    t.string "icon_size"
+    t.string "popup_anchor"
+    t.string "class_name"
   end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
--- spec/factories/iconsets.rb
+++ spec/factories/iconsets.rb
@@ -4,6 +4,10 @@ FactoryBot.define do
   factory :iconset do
     title { 'MyString' }
     text { 'MyText' }
+    icon_anchor { '[100,100]'}
+    icon_size { '[50,50]'}
+    popup_anchor { '[0,70]'}
+    class_name { 'diviconclass'}
     trait :invalid do
       title { nil }
     end
