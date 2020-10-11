ce9416c1c1b6eef36903b29fe74c01b76a403405
ut <ulf.treger@googlemail.com>
2020-10-11 17:04:14 +0200

    Adding feature trait to places to highlight featured places

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_02_124708) do
+ActiveRecord::Schema.define(version: 2020_10_11_142900) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -120,6 +120,7 @@ ActiveRecord::Schema.define(version: 2020_10_02_124708) do
     t.datetime "updated_at", null: false
     t.string "imagelink"
     t.integer "icon_id"
+    t.boolean "featured"
     t.index ["layer_id"], name: "index_places_on_layer_id"
   end
 
