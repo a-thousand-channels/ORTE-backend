5716ba3dd5f70e3fe1b996675fa573fc573ded27
ut <ulf.treger@googlemail.com>
2020-10-19 18:57:31 +0200

    Extend public JSON w/images subset

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,2 +1,12 @@
 module ImagesHelper
+  def image_url(file)
+    if file.attached?
+      polymorphic_url(file)
+    end
+  end
+  def image_linktag(file,title='')
+    if file.attached?
+      "<img src=\"#{polymorphic_path(file)}\" title=\"#{(title.present? ? title : '')}\">".html_safe
+    end
+  end
 end
