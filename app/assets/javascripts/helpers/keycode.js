35cefd803d4c72024669785763af50ee31b0341b
ut <ulf.treger@googlemail.com>
2020-12-03 19:09:51 +0100

    Keyboard shortcuts: Show hint if selection box is hidden

--- app/assets/javascripts/helpers/keycode.js
+++ app/assets/javascripts/helpers/keycode.js
@@ -4,7 +4,21 @@
 
       // toggle selection bar
       if ( ( $('body').attr('id') == 'maps' ) || ( $('body').attr('id') == 'layers' ) )  {
-       if (e.keyCode === 9) $('#selection').toggle('slow');     // tab
+        if (e.keyCode === 9) {          // tab
+          if ( $('#selection').is(":hidden") ) {
+            console.log("is hidden");
+            $('#selection').show('slow');
+            $('#selection-reveal-hint').remove();
+          } else {
+            console.log("is visible");
+            $('#selection').hide('slow');
+            $('#selection').before('<div id="selection-reveal-hint">Hit TAB to show selection box again</div>');
+            setTimeout(function(){
+              $('#selection-reveal-hint').remove();
+            }, 4500);
+
+          }
+        }
       }
       // go back one step
       if ( $('body').attr('id') == 'places' )  {
