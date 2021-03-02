c9573c29010e48d78ebcc6392808c669612c4424
ut <ulf.treger@googlemail.com>
2021-03-02 19:35:50 +0100

    Preparing for deployment w/capistrano

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,11 +1,8 @@
-# server-based syntax
-# ======================
-# Defines a single server with a list of roles and multiple properties.
-# You can define all roles on a single server, or split them:
+server 'server', roles: %w(app web db), user: 'app', port: 4199
 
-# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
-# server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
-# server "db.example.com", user: "deploy", roles: %w{db}
+set :branch, 'master'
+set :rails_env, 'staging'
+set :stage, 'staging'
 
 
 # role-based syntax
