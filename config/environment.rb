1462613fcb46e10219cab3b6db2ae05e15006a9d
ut <ulf.treger@googlemail.com>
2021-03-02 19:17:43 +0100

    Set credentials w/master key

--- config/environment.rb
+++ config/environment.rb
@@ -11,13 +11,13 @@ ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
 
       enable_starttls_auto: true,
-      address: "sslout.de",
+      address: Rails.application.credentials.dig(:mail, :address),
       port: '465',
       ssl: true,
       domain: "3plusx.de",
       authentication: :login,
-      user_name: "pm@3plusx.de",
-      password: "Test4o"
+      user_name: Rails.application.credentials.dig(:mail, :user_name),
+      password:  Rails.application.credentials.dig(:mail, :password)
 }
 
 
