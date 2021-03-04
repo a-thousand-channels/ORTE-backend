3b34ba721ed499a34f675d1cb803d0e7e6a0ee67
ut <ulf.treger@googlemail.com>
2021-03-04 13:09:08 +0100

    linked files, secrets

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,6 +20,7 @@ set :bundle_binstubs, nil
 set :notify_emails, ['']
 
 # append :linked_files, "config/secrets.yml"
+set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 
