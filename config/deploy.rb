a90224eda39563d8a3cb65681e57a9aa92ad7d3f
ut <ulf.treger@googlemail.com>
2021-03-07 19:27:02 +0100

    Adding storage to linked_dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :notify_emails, ['']
 
 # append :linked_files, "config/secrets.yml"
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/master.key')
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "storage"
 
 
 # Default branch is :master
