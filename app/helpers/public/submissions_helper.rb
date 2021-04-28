7dd41c19ccb761e122e19f2f885711b3f02c6c2e
Sefux <sfuchs@projektkater.de>
2021-04-29 00:04:50 +0200

    Add submission form

---
+++ app/helpers/public/submissions_helper.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module Public::SubmissionsHelper
+  def current_page_params
+    # Modify this list to whitelist url params for linking to the current page
+    request.params.slice("query", "filter", "sort")
+  end
+end
