ce72d22dc79f5535b02401a014328cd1e5242239
Sefux <sfuchs@projektkater.de>
2021-04-29 23:55:08 +0200

    Fix submission_config specs

--- spec/views/submission_configs/index.html.haml_spec.rb
+++ spec/views/submission_configs/index.html.haml_spec.rb
@@ -2,31 +2,40 @@ require 'rails_helper'
 
 RSpec.describe "submission_configs/index", type: :view do
   before(:each) do
+    group = FactoryBot.create(:group)
+    user = FactoryBot.create(:admin_user, group_id: group.id)
+    sign_in user
+    @map = FactoryBot.create(:map, group_id: group.id)
+    @layer = FactoryBot.create(:layer, map_id: @map.id)
+
     assign(:submission_configs, [
       SubmissionConfig.create!(
         title_intro: "Title Intro",
         subtitle_intro: "Subtitle Intro",
-        intro: "MyText",
+        intro: "MyIntro",
         title_outro: "Title Outro",
         outro: "MyText",
-        use_city_only: false
+        use_city_only: false,
+        layer_id: @layer.id
       ),
       SubmissionConfig.create!(
         title_intro: "Title Intro",
         subtitle_intro: "Subtitle Intro",
-        intro: "MyText",
+        intro: "MyIntro",
         title_outro: "Title Outro",
         outro: "MyText",
-        use_city_only: false
+        use_city_only: false,
+        layer_id: @layer.id
       )
     ])
   end
 
   it "renders a list of submission_configs" do
     render
+    
     assert_select "tr>td", text: "Title Intro".to_s, count: 2
     assert_select "tr>td", text: "Subtitle Intro".to_s, count: 2
-    assert_select "tr>td", text: "MyText".to_s, count: 2
+    assert_select "tr>td", text: "MyIntro".to_s, count: 2
     assert_select "tr>td", text: "Title Outro".to_s, count: 2
     assert_select "tr>td", text: "MyText".to_s, count: 2
     assert_select "tr>td", text: false.to_s, count: 2
