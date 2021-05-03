a878e6e8c1f999d5d6e163fae8c99b88a6b7c9d9
Sefux <sfuchs@projektkater.de>
2021-05-04 01:40:56 +0200

    Add spec: submission_config model

--- spec/models/submission_config_spec.rb
+++ spec/models/submission_config_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe SubmissionConfig, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    expect(build(:submission_config)).to be_valid
+  end
+
 end
