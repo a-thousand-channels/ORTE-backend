ed062993318c0a72a973cc60eedd5f0258c3ff58
ut <ulf.treger@googlemail.com>
2021-07-26 22:58:13 +0200

    deployment errors: Shorten index names/3

--- db/migrate/20210429230553_create_string_translations.rb
+++ db/migrate/20210429230553_create_string_translations.rb
@@ -8,9 +8,9 @@ class CreateStringTranslations < ActiveRecord::Migration[5.2]
         t.references :translatable, polymorphic: true, index: false
         t.timestamps null: false
       end
+      add_index :mobility_string_translations, [:translatable_id, :translatable_type, :locale, :key], unique: true, name: :index_mobility_string_translations_on_keys
+      add_index :mobility_string_translations, [:translatable_id, :translatable_type, :key], name: :index_mobility_string_translate_translatable_attribute
+      add_index :mobility_string_translations, [:translatable_type, :key, :value, :locale], name: :index_mobility_string_translate_query_keys
     end
-    add_index :mobility_string_translations, [:translatable_id, :translatable_type, :locale, :key], unique: true, name: :index_mobility_string_translations_on_keys
-    add_index :mobility_string_translations, [:translatable_id, :translatable_type, :key], name: :index_mobility_string_translate_translatable_attribute
-    add_index :mobility_string_translations, [:translatable_type, :key, :value, :locale], name: :index_mobility_string_translate_query_keys
   end
 end
