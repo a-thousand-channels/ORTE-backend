712e5aec1f99fa651a7415484f53b904e9e56001
ut <ulf.treger@googlemail.com>
2021-11-02 21:59:56 +0100

    Annotations: People MVC by scaffolding

---
+++ spec/views/people/edit.html.haml_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'people/edit', type: :view do
+  before(:each) do
+    @person = assign(:person, Person.create!(
+                                name: 'MyString',
+                                info: 'MyText'
+                              ))
+  end
+
+  it 'renders the edit person form' do
+    render
+
+    assert_select 'form[action=?][method=?]', person_path(@person), 'post' do
+      assert_select 'input[name=?]', 'person[name]'
+
+      assert_select 'textarea[name=?]', 'person[info]'
+    end
+  end
+end
