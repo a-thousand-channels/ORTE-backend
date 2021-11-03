db49b6158c3c8e4e789e43c6038e897262de2918
ut <ulf.treger@googlemail.com>
2021-11-03 22:00:06 +0100

    Annotations: Improve new/edit flow

--- app/models/annotation.rb
+++ app/models/annotation.rb
@@ -1,8 +1,9 @@
 class Annotation < ApplicationRecord
   belongs_to :place
-  belongs_to :person
+  belongs_to :person, optional: true
 
-  validates :title, presence: true
+  validates :title, uniqueness: true
+  validates :text, presence: true
 
   scope :published, -> { where(published: true) }
 end
