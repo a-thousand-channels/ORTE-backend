f126e36ba90d5062c33e7bd61277d015fc9c3567
ut <ulf.treger@googlemail.com>
2021-11-06 23:34:39 +0100

    Annotations: Improve form

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_02_193259) do
+ActiveRecord::Schema.define(version: 2021_11_06_221932) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -37,24 +37,14 @@ ActiveRecord::Schema.define(version: 2021_11_02_193259) do
     t.string "title"
     t.text "text"
     t.bigint "place_id"
+    t.bigint "person_id"
     t.boolean "published", default: false
     t.integer "sorting"
     t.text "source"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "person_id"
-    t.index ["place_id"], name: "index_annotations_on_place_id"
-  end
-
-  create_table "friendly_id_slugs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "slug", null: false
-    t.integer "sluggable_id", null: false
-    t.string "sluggable_type", limit: 50
-    t.string "scope"
-    t.datetime "created_at"
-    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, length: { slug: 70, scope: 70 }
-    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", length: { slug: 140 }
-    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
+    t.index ["person_id"], name: "fk_rails_adeffa1c70"
+    t.index ["place_id"], name: "fk_rails_51dbcfe977"
   end
 
   create_table "groups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -65,7 +55,7 @@ ActiveRecord::Schema.define(version: 2021_11_02_193259) do
 
   create_table "icons", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
-    t.integer "iconset_id"
+    t.bigint "iconset_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["iconset_id"], name: "index_icons_on_iconset_id"
@@ -87,7 +77,7 @@ ActiveRecord::Schema.define(version: 2021_11_02_193259) do
     t.string "licence"
     t.text "source"
     t.string "creator"
-    t.integer "place_id"
+    t.bigint "place_id"
     t.string "alt"
     t.string "caption"
     t.integer "sorting"
@@ -101,7 +91,7 @@ ActiveRecord::Schema.define(version: 2021_11_02_193259) do
     t.string "title"
     t.string "subtitle"
     t.boolean "published"
-    t.integer "map_id"
+    t.bigint "map_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "color"
@@ -116,7 +106,7 @@ ActiveRecord::Schema.define(version: 2021_11_02_193259) do
     t.string "title"
     t.string "subtitle"
     t.boolean "published"
-    t.integer "group_id"
+    t.bigint "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "script"
@@ -178,12 +168,13 @@ ActiveRecord::Schema.define(version: 2021_11_02_193259) do
     t.string "city"
     t.string "country"
     t.boolean "published"
-    t.integer "layer_id"
+    t.bigint "layer_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "imagelink"
     t.integer "icon_id"
     t.boolean "featured"
+    t.string "ptype", default: "info"
     t.index ["layer_id"], name: "index_places_on_layer_id"
   end
 
@@ -255,9 +246,9 @@ ActiveRecord::Schema.define(version: 2021_11_02_193259) do
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
     t.string "role", default: "user"
-    t.integer "group_id"
-    t.datetime "created_at", default: "2021-06-28 17:11:21", null: false
-    t.datetime "updated_at", default: "2021-06-28 17:11:21", null: false
+    t.bigint "group_id"
+    t.datetime "created_at", default: "2021-09-03 18:29:24", null: false
+    t.datetime "updated_at", default: "2021-09-03 18:29:24", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["group_id"], name: "index_users_on_group_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
@@ -278,9 +269,16 @@ ActiveRecord::Schema.define(version: 2021_11_02_193259) do
     t.index ["place_id"], name: "index_videos_on_place_id"
   end
 
+  add_foreign_key "annotations", "people"
   add_foreign_key "annotations", "places"
+  add_foreign_key "icons", "iconsets"
+  add_foreign_key "images", "places"
+  add_foreign_key "layers", "maps"
+  add_foreign_key "maps", "groups"
+  add_foreign_key "places", "layers"
   add_foreign_key "submission_configs", "layers"
   add_foreign_key "submissions", "places"
   add_foreign_key "taggings", "tags"
+  add_foreign_key "users", "groups"
   add_foreign_key "videos", "places"
 end
