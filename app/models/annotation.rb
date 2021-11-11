15644cd9e12be0726b3e79db8da513ef0c72d504
ut <ulf.treger@googlemail.com>
2021-11-11 13:25:20 +0100

    Annotations: Adding audio to an annotation #130

--- app/models/annotation.rb
+++ app/models/annotation.rb
@@ -1,10 +1,16 @@
+# frozen_string_literal: true
+
 class Annotation < ApplicationRecord
   belongs_to :place
   belongs_to :person, optional: true
 
   acts_as_taggable_on :tags
 
+  has_one_attached :audio, dependent: :destroy
+
   validates :text, presence: true
+  validate :check_audio_format
+
 
   scope :published, -> { where(published: true) }
 
@@ -13,5 +19,15 @@ class Annotation < ApplicationRecord
     if person
       "#{person.name}"
     end
+  end
+
+  def audiolink
+    ApplicationController.helpers.audio_link(audio) if audio
+  end
+
+  private
+
+  def check_audio_format
+    errors.add(:audio, 'format must be MP3.') if audio.attached? && !audio.content_type.in?(%w[audio/mpeg])
   end
 end
