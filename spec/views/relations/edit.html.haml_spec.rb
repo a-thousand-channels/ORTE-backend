3facc51b6df2dce528403bdd9c5504c087dbc371
ut <ulf.treger@googlemail.com>
2021-11-26 17:28:42 +0100

    Relations: Specs

--- spec/views/relations/edit.html.haml_spec.rb
+++ spec/views/relations/edit.html.haml_spec.rb
@@ -2,13 +2,18 @@ require 'rails_helper'
 
 RSpec.describe "relations/edit", type: :view do
   before(:each) do
-    @relation = assign(:relation, Relation.create!())
+    @map = create(:map)
+    layer = create(:layer, map:@map)
+    @layers = [layer]
+    p1 = create(:place)
+    p2 = create(:place)
+    @relation = create(:relation, relation_from_id: p1.id, relation_to_id: p2.id)
   end
 
   it "renders the edit relation form" do
     render
 
-    assert_select "form[action=?][method=?]", relation_path(@relation), "post" do
+    assert_select "form[action=?][method=?]", map_relation_path(@map,@relation), "post" do
     end
   end
 end
