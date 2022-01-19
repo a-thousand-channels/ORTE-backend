69695ad2c4e7a40d4b0932798bc7f858d833b0f2
Sefux <sfuchs@projektkater.de>
2022-01-19 18:33:45 +0100

    Fix specs (refs #170)

--- spec/services/build/maptogo_spec.rb
+++ spec/services/build/maptogo_spec.rb
@@ -6,18 +6,21 @@ RSpec.describe Build::Maptogo do
   describe '#run' do
     before do
       @build_config = Rails.application.config_for('build/maptogo')
+      @group = FactoryBot.create(:group)
       @user = FactoryBot.create(:admin_user, email: 'user99@example.com')
+      @map = FactoryBot.create(:map, group_id: @group.id)
+      @layer = FactoryBot.create(:layer, map_id: @map.id, published: true)
     end
 
     it 'returns created_at' do
       freeze_time do
-        result = Build::Maptogo.new(@user).build
+        result = Build::Maptogo.new(@user, @map, @layer).build
 
         expect(result[:meta][:created_at]).to eq DateTime.current.to_s
       end
     end
     it 'returns version' do
-      result = Build::Maptogo.new(@user).build
+      result = Build::Maptogo.new(@user, @map, @layer).build
 
       expect(result[:meta][:version]).to eq @build_config['version'].to_s
     end
