7412de6e8140902e3f5d48f766d9b4cba83edc3e
ut <ulf.treger@googlemail.com>
2022-01-21 13:11:36 +0100

    Provide image path and image filename to public layer json

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -10,6 +10,15 @@ module ImagesHelper
     polymorphic_url(file.variant(resize: '800x800').processed)
   end
 
+  def image_path(file)
+    return unless file.attached?
+
+    filename = ActiveStorage::Blob.service.path_for(file.key)
+    return unless File.exist?(filename)
+
+    polymorphic_path(file.variant(resize: '800x800').processed)
+  end
+
   def image_linktag(file, title = '')
     return unless file.attached?
 
