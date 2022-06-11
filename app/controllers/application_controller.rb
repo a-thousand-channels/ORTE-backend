89d3ae6c4ee92266fe7062a9a79cd15ba6b85adf
Ulf Treger <ulf.treger@googlemail.com>
2022-06-11 19:05:05 +0200

    Rubocop'd

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,12 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-  rescue_from ActionController::RoutingError, :with => :error_render_method
-  rescue_from ActiveRecord::RecordNotFound, :with => :error_render_method
+  rescue_from ActionController::RoutingError, with: :error_render_method
+  rescue_from ActiveRecord::RecordNotFound, with: :error_render_method
 
   def error_render_method
     puts '#####################################'
-    puts "Raise Error"
+    puts 'Raise Error'
     respond_to do |type|
       # type.xml { render :template => "errors/error_404", :status => 404 }
       # type.all  { render :nothing => true, :status => 404 }
