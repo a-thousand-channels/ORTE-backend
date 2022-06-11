bbed32eba0e3df5128e30ac189cfbb6b89080cd5
Ulf Treger <ulf.treger@googlemail.com>
2022-06-11 18:37:16 +0200

    Implement a 404/Notfound page, eg for relation controller fallback

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,21 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
+  rescue_from ActionController::RoutingError, :with => :error_render_method
+  rescue_from ActiveRecord::RecordNotFound, :with => :error_render_method
+
+  def error_render_method
+    puts '#####################################'
+    puts "Raise Error"
+    respond_to do |type|
+      # type.xml { render :template => "errors/error_404", :status => 404 }
+      # type.all  { render :nothing => true, :status => 404 }
+      # type.all  { render :template => 'start/404', :status => 404 }
+      type.all { redirect_to notfound_url }
+    end
+    false
+  end
+
   def report_csp
     # do nothing right now...
   end
