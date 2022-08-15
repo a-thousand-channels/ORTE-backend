ccfae1f77c80ac152e537fbd2ec228c83e771a0a
ut <ulf.treger@googlemail.com>
2022-08-15 18:09:38 +0200

    BuildLogs controller specs

--- app/controllers/build_logs_controller.rb
+++ app/controllers/build_logs_controller.rb
@@ -3,18 +3,14 @@
 class BuildLogsController < ApplicationController
   before_action :set_build_log, only: %i[show edit update destroy]
 
-  # GET /build_logs or /build_logs.json
   def index
     @build_logs = BuildLog.all
   end
 
-  # GET /build_logs/1 or /build_logs/1.json
   def show; end
 
-  # GET /build_logs/1/edit
   def edit; end
 
-  # POST /build_logs or /build_logs.json
   def create
     @build_log = BuildLog.new(build_log_params)
 
@@ -23,13 +19,12 @@ class BuildLogsController < ApplicationController
         format.html { redirect_to @build_log, notice: 'Build log was successfully created.' }
         format.json { render :show, status: :created, location: @build_log }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html { render :show, status: :unprocessable_entity }
         format.json { render json: @build_log.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /build_logs/1 or /build_logs/1.json
   def update
     respond_to do |format|
       if @build_log.update(build_log_params)
@@ -42,7 +37,6 @@ class BuildLogsController < ApplicationController
     end
   end
 
-  # DELETE /build_logs/1 or /build_logs/1.json
   def destroy
     @build_log.destroy
     respond_to do |format|
@@ -53,14 +47,12 @@ class BuildLogsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_build_log
     @build_log = BuildLog.find(params[:id])
     @map = Map.by_user(current_user).find_by_slug(params[:map_id]) || Map.by_user(current_user).find_by_id(params[:map_id])
     @layer = Layer.find_by_slug(params[:layer_id]) || Layer.find_by_id(params[:layer_id])
   end
 
-  # Only allow a list of trusted parameters through.
   def build_log_params
     params.require(:build_log).permit(:map_id, :layer_id, :output, :size, :version)
   end
