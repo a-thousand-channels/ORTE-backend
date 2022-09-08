38c7d4bf71bcbd2f97c402467f7f570f07854984
Ulf Treger <ulf.treger@googlemail.com>
2022-09-08 13:47:54 +0200

    Additionally add a feature test for tinymce working

--- spec/features/places_feature_spec.rb
+++ spec/features/places_feature_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.feature 'User create and updates a place ' do
+RSpec.feature 'User creates and updates a place ' do
   before do
     @group = FactoryBot.create(:group)
     user = FactoryBot.create(:admin_user, group_id: @group.id)
@@ -24,5 +24,22 @@ RSpec.feature 'User create and updates a place ' do
     visit edit_map_layer_place_path(@map, @layer, @place)
     expect(page).to have_content 'my layer'
     expect(page).not_to have_content 'other layer'
+  end
+
+  scenario 'shows edit form (and place some text into a tinymce textarea filed)', js: true do
+    @map = FactoryBot.create(:map, group_id: @group.id)
+    @layer = FactoryBot.create(:layer, map_id: @map.id, title: 'my layer')
+
+    @other_group = FactoryBot.create(:group)
+    @other_map = FactoryBot.create(:map, group_id: @other_group.id)
+    @other_layer = FactoryBot.create(:layer, map_id: @other_map.id, title: 'other layer')
+    @place = FactoryBot.create(:place, layer_id: @layer.id)
+    visit edit_map_layer_place_path(@map, @layer, @place)
+    expect(page).to have_css('div.tox-tinymce')
+    expect(page).to have_css('iframe#place_teaser_ifr')
+    page.execute_script('tinymce.get("place_teaser").setContent("updated content");')
+    within_frame("place_teaser_ifr") do
+      expect(page).to have_css('body#tinymce p',  text: 'updated content');
+    end
   end
 end
