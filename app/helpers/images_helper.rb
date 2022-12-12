a081516a13de339208ee1c2a2779101a62197508
Ulf Treger <ulf.treger@googlemail.com>
2022-12-12 11:56:28 +0100

    Rescue possible ActiveStorage errors for images > image_url/2

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -8,19 +8,35 @@ module ImagesHelper
   def image_url(file)
     return unless file.attached?
 
-    filename = ActiveStorage::Blob.service.path_for(file.key)
-    return unless File.exist?(filename)
+    begin
+      filename = ActiveStorage::Blob.service.path_for(file.key)
+      return unless File.exist?(filename)
 
-    polymorphic_url(file.variant(resize: '800x800').processed)
+      polymorphic_url(file.variant(resize: '800x800').processed)
+    rescue Errno::ENOENT => e
+      ''
+    rescue ActiveStorage::FileNotFoundError => e
+      ''
+    rescue ActiveStorage::IntegrityError => e
+      ''
+    end
   end
 
   def image_path(file)
     return unless file.attached?
 
-    filename = ActiveStorage::Blob.service.path_for(file.key)
-    return unless File.exist?(filename)
+    begin
+      filename = ActiveStorage::Blob.service.path_for(file.key)
+      return unless File.exist?(filename)
 
-    polymorphic_path(file.variant(resize: '800x800').processed)
+      polymorphic_path(file.variant(resize: '800x800').processed)
+    rescue Errno::ENOENT => e
+      ''
+    rescue ActiveStorage::FileNotFoundError => e
+      ''
+    rescue ActiveStorage::IntegrityError => e
+      ''
+    end
   end
 
   def image_linktag(file, title = '')
