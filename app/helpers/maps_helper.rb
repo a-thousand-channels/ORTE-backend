f76e9f29f1c3cb18347d36d9563a51bbce5cd7f9
Ulf Treger <ulf.treger@googlemail.com>
2023-02-20 13:37:47 +0100

    Rubocop'd

--- app/helpers/maps_helper.rb
+++ app/helpers/maps_helper.rb
@@ -3,11 +3,11 @@
 module MapsHelper
   def popup_display_mode_for_select
     popup_display_modes = %w[click click+mouseover]
-    popup_display_modes.each_with_object({}) { |e, m| m[e.capitalize] = e; }
+    popup_display_modes.each_with_object({}) { |e, m| m[e.capitalize] = e }
   end
 
   def marker_display_mode_for_select
     marker_display_modes = %w[cluster single]
-    marker_display_modes.each_with_object({}) { |e, m| m[e.capitalize] = e; }
+    marker_display_modes.each_with_object({}) { |e, m| m[e.capitalize] = e }
   end
 end
