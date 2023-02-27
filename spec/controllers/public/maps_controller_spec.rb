36a82a9b2e0ae1a187052961e3bef974727df762
Ulf Treger <ulf.treger@googlemail.com>
2023-02-27 19:10:24 +0100

    Add some testing

--- spec/controllers/public/maps_controller_spec.rb
+++ spec/controllers/public/maps_controller_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe Public::MapsController, type: :controller do
   render_views
 
-  describe "functionalities with logged in user with role 'admin'" do
+  describe "functionalities with anonymous access" do
     before do
       @group = FactoryBot.create(:group)
       user = FactoryBot.create(:admin_user, group_id: @group.id)
@@ -62,12 +62,50 @@ RSpec.describe Public::MapsController, type: :controller do
         expect(response).to match_response_schema('map', 'json')
       end
 
-      xit 'returns an 403 + error response for unpublished resources' do
+      it 'returns an 403 + error response for unpublished resources' do
         map = Map.create! invalid_attributes
         get :show, params: { id: map.friendly_id, format: 'json' }, session: valid_session
         expect(response).to have_http_status(403)
         expect(JSON.parse(response.body)['error']).to match(/Map not accessible/)
       end
     end
+
+    describe 'GET #allplaces' do
+      it 'returns a success response for a published map' do
+        map = Map.create! valid_attributes
+        layer = FactoryBot.create(:layer, map_id: map.id, published: true)
+        place = FactoryBot.create(:place, layer_id: layer.id, published: true)
+        get :allplaces, params: { id: map.friendly_id, format: 'json' }, session: valid_session
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns json for a published map' do
+        map = Map.create! valid_attributes
+        layer = FactoryBot.create(:layer, map_id: map.id, published: true)
+        place = FactoryBot.create(:place, layer_id: layer.id, published: true)
+        get :allplaces, params: { id: map.friendly_id, format: 'json' }, session: valid_session
+        expect(response).to have_http_status(200)
+        expect(response.status).to eq 200
+        expect(response.content_type).to eq('application/json; charset=utf-8')
+      end
+
+      it 'returns json with a valid scheme' do
+        map = Map.create! valid_attributes
+        layer = FactoryBot.create(:layer, map_id: map.id, published: true)
+        place = FactoryBot.create(:place, layer_id: layer.id, published: true)
+        get :allplaces, params: { id: map.friendly_id, format: 'json' }, session: valid_session
+        expect(response).to match_response_schema('map_allplaces', 'json')
+      end
+
+      it 'returns an 403 + error response for unpublished resources' do
+        map = Map.create! invalid_attributes
+        layer = FactoryBot.create(:layer, map_id: map.id, published: true)
+        place = FactoryBot.create(:place, layer_id: layer.id, published: true)
+        get :allplaces, params: { id: map.friendly_id, format: 'json' }, session: valid_session
+        expect(response).to have_http_status(403)
+        expect(JSON.parse(response.body)['error']).to match(/Map not accessible/)
+      end
+    end
+
   end
 end
