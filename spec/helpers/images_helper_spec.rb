1fc5e61d823026d30a766713bead1d583653ca5a
Ulf Treger <ulf.treger@googlemail.com>
2023-04-08 20:56:52 +0200

    Improve code coverage for places and images model.

--- spec/helpers/images_helper_spec.rb
+++ spec/helpers/images_helper_spec.rb
@@ -6,16 +6,20 @@ RSpec.describe ImagesHelper, type: :helper do
   describe 'image_url' do
     it 'it returns an polymorphic image link' do
       p = create(:place)
-      i = create(:image, place: p)
+      i = build(:image, place: p)
       i.file.attach(io: File.open(Rails.root.join('spec', 'support', 'files', 'test.jpg')), filename: 'attachment.jpg', content_type: 'image/jepg')
+      i.save!
+      i.reload
       expect(helper.image_url(i.file)).to eq(polymorphic_url(i.file.variant(resize: '800x800').processed).to_s)
     end
   end
   describe 'image_linktag' do
     it 'it returns an image link tag' do
       p = create(:place)
-      i = create(:image, place: p)
+      i = build(:image, place: p)
       i.file.attach(io: File.open(Rails.root.join('spec', 'support', 'files', 'test.jpg')), filename: 'attachment.jpg', content_type: 'image/jpeg')
+      i.save!
+      i.reload
       expect(helper.image_linktag(i.file)).to eq("<img src=\"#{polymorphic_url(i.file.variant(resize: '800x800').processed)}\" title=\"\">")
     end
   end
--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -98,8 +98,9 @@ RSpec.describe Place, type: :model do
     p1 = FactoryBot.create(:place, layer: l)
     expect(p1.imagelink2).not_to eq(p1.imagelink)
     p2 = FactoryBot.create(:place, layer: l)
-    i = create(:image, :preview, place: p2)
+    i = build(:image, :preview, place: p2)
     i.file.attach(io: File.open(Rails.root.join('spec', 'support', 'files', 'test.jpg')), filename: 'attachment.jpg', content_type: 'image/jepg')
+    i.save!
     expect(p2.imagelink2).not_to eq(p2.imagelink)
   end
 
