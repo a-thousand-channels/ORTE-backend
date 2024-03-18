0b3c9dc2ce6b17a14645f56002cffb85e049fabf
Ulf Treger <ulf.treger@googlemail.com>
2024-03-18 20:01:12 +0100

    Adding basic timeslider behaviour on the index list (refs #301)

--- app/assets/javascripts/helpers/timeslider.js
+++ app/assets/javascripts/helpers/timeslider.js
@@ -5,9 +5,32 @@ jQuery(function ($) {
   $(document).on('changed.zf.slider', '[data-slider]', function(event) {
     var $slider = $(event.currentTarget);
     var $numberInput = $slider.children('input');
-    console.log($numberInput.val())
-    $('#slider-selection').html($numberInput.val())
-    // TODO: create list of all places with startyear and endyear data values
+    let year = $numberInput.val()
+    console.log(year)
+    $('#slider-selection').html("Year selected: "+year);
+    
+    // get all places with id + startyear data values  
+    let places_with_years = $('#timeline .place-entry').map(function() {
+      return {
+        id: $(this).attr('id'),
+        startyear: $(this).data('startyear'),
+        endyear: $(this).data('endyear')
+      }
+    })
+    console.log(places_with_years);
+
+    places_with_years.each(function() {
+      console.log(this.id, this.startyear, this.endyear)
+      if (year >= this.startyear && year <= this.endyear) {
+        console.log('show')
+        $('#'+this.id).show()
+      } else {
+        console.log('hide')
+        $('#'+this.id).hide()
+      }
+    });
+
+  
     // hide all
     // show only those existing in the selected year
   });
