10e2565180f36542cb7d24240a4a8b5cbe89109f
Ulf Treger <ulf.treger@googlemail.com>
2024-03-22 16:15:45 +0100

    Improve smart_date_display_with_qualifier helper and to some helper testing (refs #313)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -52,7 +52,7 @@ module ApplicationHelper
     end
   end
 
-  def smart_date_display_with_qualifier(startdate, enddate, startdate_qualifier, enddate_qualifier)
+  def smart_date_display_with_qualifier(startdate, enddate, startdate_qualifier = '', enddate_qualifier = '')
     return unless startdate
 
     if !enddate
@@ -64,22 +64,22 @@ module ApplicationHelper
           if startdate.strftime('%d.%m') == '01.01'
             startdate.strftime('%Y').to_s
           else
-            startdate.strftime('%d.%m.%y').to_s
+            startdate.strftime('%-d.%-m.%y').to_s
           end
         else
-          startdate.strftime('%d.%m.%y, %H:%M').to_s
+          startdate.strftime('%-d.%-m.%y, %H:%M').to_s
         end
       end
     elsif startdate > enddate
-      startdate.strftime('%d.%m.%y, %H:%M').to_s
+      startdate.strftime('%-d.%-m.%y, %H:%M').to_s
     elsif startdate == enddate
       if startdate.strftime('%H:%M') == '00:00'
-        startdate.strftime('%d.%m.%y').to_s
+        startdate.strftime('%-d.%-m.%y').to_s
       else
-        startdate.strftime('%d.%m.%y, %H:%M').to_s
+        startdate.strftime('%-d.%-m.%y, %H:%M').to_s
       end
     elsif startdate.to_date == enddate.to_date
-      "#{startdate.strftime('%d.%m.%y, %H:%M')} ‒ #{enddate.strftime('%H:%M')}"
+      "#{startdate.strftime('%-d.%-m.%y, %H:%M')} ‒ #{enddate.strftime('%H:%M')}"
     else
       if startdate_qualifier == 'circa'
         if enddate_qualifier == 'circa'
@@ -97,26 +97,32 @@ module ApplicationHelper
           else
             "ca. #{startdate.strftime('%Y')} ‒ #{enddate.strftime('%Y')}"
           end
+        elsif enddate_qualifier == 'exact'
+          # (~ Ca. 2010 ‒ 15.10.2024)
+          "ca. #{startdate.strftime('%Y')} ‒ #{enddate.strftime('%-d.%-m.%Y')}"
+          # return date with no leading zero
+
         else
           if enddate.strftime('%d.%m') == '01.01'
             # (~ ca. 2010 ‒ 2024)
             "ca. #{startdate.strftime('%Y')} ‒ #{enddate.strftime('%Y')}"
           else
             # (~ Ca. 2010 ‒ 15.10.2024)
-            "ca. #{startdate.strftime('%Y')} ‒ #{enddate.strftime('%d.%m.%Y')}"
+            "ca. #{startdate.strftime('%Y')} ‒ #{enddate.strftime('%-d.%-m.%Y')}"
           end
         end
       elsif enddate_qualifier == 'circa'
         if startdate_qualifier == 'exact'
-          "#{startdate.strftime('%d.%m.%Y')}  ‒ ca. #{enddate.strftime('%Y')}"
+          # ~ 15.6.2000 ‒ ca. 2008
+          "#{startdate.strftime('%-d.%-m.%Y')} ‒ ca. #{enddate.strftime('%Y')}"
         else
           if startdate.strftime('%d.%m') == '01.01'
             if (enddate.strftime('%Y').to_i % 10).zero?
               # ~ from 1992 to 2000s
-              "from #{startdate.strftime('%Y')}  to #{enddate.strftime('%Y')}s"
+              "from #{startdate.strftime('%Y')} to #{enddate.strftime('%Y')}s"
             else
               # ~ from 1992 to ca. 2008
-              "from #{startdate.strftime('%Y')}  to ca. #{enddate.strftime('%Y')}"
+              "from #{startdate.strftime('%Y')} to ca. #{enddate.strftime('%Y')}"
             end
           end
         end
@@ -128,10 +134,10 @@ module ApplicationHelper
           elsif (startdate.strftime('%d.%m') == '01.01' && enddate.strftime('%d.%m') == '31.12') && (startdate.strftime('%Y') == enddate.strftime('%Y'))
             startdate.strftime('%Y').to_s
           else
-            "#{startdate.strftime('%d.%m.%y')} ‒ #{enddate.strftime('%d.%m.%y')}"
+            "#{startdate.strftime('%-d.%-m.%y')} ‒ #{enddate.strftime('%-d.%-m.%y')}"
           end
         else
-          "#{startdate.strftime('%d.%m.%y, %H:%M')} ‒ #{enddate.strftime('%d.%m.%y, %H:%M')}"
+          "#{startdate.strftime('%-d.%-m.%y, %H:%M')} ‒ #{enddate.strftime('%-d.%-m.%y, %H:%M')}"
         end
       end
     end
