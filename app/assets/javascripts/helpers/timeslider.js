0525d79d2cd380c3c899a222bb1bc3bacbbf4859
Ulf Treger <ulf.treger@3plusx.de>
2024-05-15 20:05:32 +0200

    Enable date by year calculations for filtering (#refs 301)

--- app/assets/javascripts/helpers/timeslider.js
+++ app/assets/javascripts/helpers/timeslider.js
@@ -1,54 +1,68 @@
 jQuery(function ($) {
-// scrollbuttons
-let scrollLeft = document.createElement('div');
-scrollLeft.setAttribute('id', 'scroll-left');
-scrollLeft.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M11.8284 12.0005L14.6569 14.8289L13.2426 16.2431L9 12.0005L13.2426 7.75781L14.6569 9.17203L11.8284 12.0005Z"></path></svg>';
-let scrollRight = document.createElement('div');
-scrollRight.setAttribute('id', 'scroll-right');
-scrollRight.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M12.1717 12.0005L9.34326 9.17203L10.7575 7.75781L15.0001 12.0005L10.7575 16.2431L9.34326 14.8289L12.1717 12.0005Z"></path></svg>';
+
+  let body = document.querySelector("body");
+
+  if ( body.classList.contains("show") && ( body.id == 'maps') ) {
+
+    // scrollbuttons
+    let scrollLeft = document.createElement('div');
+    scrollLeft.setAttribute('id', 'scroll-left');
+    scrollLeft.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M11.8284 12.0005L14.6569 14.8289L13.2426 16.2431L9 12.0005L13.2426 7.75781L14.6569 9.17203L11.8284 12.0005Z"></path></svg>';
+    let scrollRight = document.createElement('div');
+    scrollRight.setAttribute('id', 'scroll-right');
+    scrollRight.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M12.1717 12.0005L9.34326 9.17203L10.7575 7.75781L15.0001 12.0005L10.7575 16.2431L9.34326 14.8289L12.1717 12.0005Z"></path></svg>';
+
+    // timeline
+    let timelineWrapper = document.createElement('div');
+    timelineWrapper.setAttribute('id', 'timeline-wrapper');
+    timelineWrapper.appendChild(scrollLeft);
+    timelineWrapper.appendChild(scrollRight);
+    let timelineContent = document.createElement('div');
+    timelineContent.setAttribute('id', 'timeline-content');
+    timelineWrapper.appendChild(timelineContent);
+    let body = document.querySelector("body");
+    document.body.insertBefore(timelineWrapper, document.querySelector("footer"));
 
 
-// time line
-let timelineWrapper = document.createElement('div');
-timelineWrapper.setAttribute('id', 'timeline-wrapper');
-timelineWrapper.appendChild(scrollLeft);
-timelineWrapper.appendChild(scrollRight);
-let timelineContent = document.createElement('div');
-timelineContent.setAttribute('id', 'timeline-content');
-timelineWrapper.appendChild(timelineContent);
-let body = document.querySelector("body");
-document.body.insertBefore(timelineWrapper, document.querySelector("footer"));
+    let minYear = 2000;
+    let maxYear = 2024;
 
+    let diff = maxYear - minYear;
+    let step = 1;
 
-let minYear = 2000;
-let maxYear = 2024;
+    for (var i = minYear; i <= maxYear; i += step) {
+      var div = document.createElement('div');
+      div.classList.add('year');
+      div.setAttribute('id', 'year'+i);
+      div.setAttribute('data-year', i);
+      div.textContent = i;
+      timelineContent.appendChild(div);
+    }
 
-let diff = maxYear - minYear;
-let step = 1;
-
-for (var i = minYear; i <= maxYear; i += step) {
-  var div = document.createElement('div');
-  div.classList.add('year');
-  div.setAttribute('id', 'year'+i);
-  div.setAttribute('data-year', i);
-  div.textContent = i;
-  timelineContent.appendChild(div);
-}
+  }
 });
-
+let current_selected_year = 1900;
 function filterMarkers(selectedYear) {
   console.log("filterMarkers **************");
-  console.log(window.markers_layer);
-  window.markers_layer.forEach(function(marker) {
+  console.log(window.markers);
+  
+
+  const status = document.getElementById("timeline-info-status");
+  status.innerHTML = "Selected Year "+selectedYear;
+
+  window.markers.forEach(function(marker) {
     if (marker.data.fromYear <= selectedYear && marker.data.endYear >= selectedYear) {
-        marker.addTo(map);
-        marker.setIcon(icon);
+        console.log(marker.data.fromYear,selectedYear,marker.data.endYear);
+        marker.addTo(window.map);
+        marker.setIcon(window.icon);
     } else {       
       if ( selectedYear <= current_selected_year ) {
-        map.removeLayer(marker);  
+        window.map.removeLayer(marker);  
         // marker.setIcon(icon_past);
       } else {
-        marker.setIcon(icon_past);
+        window.map.removeLayer(marker);  
+        window.icon_past = window.icon;
+        marker.setIcon(window.icon_past);
       }    
     }
   });
@@ -96,7 +110,7 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
   el = document.getElementById('year2008'); 
-  SelectAndFilterByYear(el,yearDivs,'2008')
+  // SelectAndFilterByYear(el,yearDivs,'2008')
 });
 
 
