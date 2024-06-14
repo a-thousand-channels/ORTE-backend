44b6c7209c29010f4482c537595e21db4e2bfca1
Ulf Treger <ulf.treger@3plusx.de>
2024-06-14 20:29:58 +0200

    2 Fixes (refs #301)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -33,11 +33,14 @@ end
 
 Capybara.register_driver :headless_chrome do |app|
   options = ::Selenium::WebDriver::Chrome::Options.new
+
   options.add_argument('--headless')
   options.add_argument('--no-sandbox')
   options.add_argument('--disable-dev-shm-usage')
   options.add_argument('--window-size=1400,1400')
   options.add_argument('--disable-features=VizDisplayCompositor')
+  options.add_argument('--enable-logging') # Enables logging
+  options.add_argument('--log-level=0') # Enables all logging
 
   Capybara::Selenium::Driver.new app,
                                  browser: :chrome,
