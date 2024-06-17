52b6d6fc6c229f6b1efdacd759c0f2f6dc01a3b9
Ulf Treger <ulf.treger@3plusx.de>
2024-06-17 20:22:43 +0200

    Two fixes

--- app/assets/javascripts/helpers/timeslider.js
+++ app/assets/javascripts/helpers/timeslider.js
@@ -155,11 +155,11 @@ function filterMarkers(selectedYear,places) {
         console.log("X Past", marker.data.fromYear,selectedYear,marker.data.endYear,marker.data.color, marker.data.title, marker.data.layer_id);        
 
         if ( $('#map').hasClass('darken-icons2') ) {
-          icon = LargeMarkerIcon.create({color: '#222', opacity: 0.2});
+          icon = LargeMarkerIcon.create({color: '#333', opacity: 0.2});
         } else if ( $('#map').hasClass('darken-icons') ) {
           icon = LargeMarkerIcon.create({color: '#555', opacity: 0.2});
         } else {
-          icon = LargeMarkerIcon.create({color: '#fff', opacity: 0.3});
+          icon = LargeMarkerIcon.create({color: '#fff', opacity: 0.42});
         }
         window.map.removeLayer(marker);  
         marker.setIcon(icon);    
