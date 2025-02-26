2ee93c1815239608a068a06e0a6ac75547fd306e
Birte Goldt <bigo@akra.de>
2025-02-26 15:43:05 +0100

    fix: remove rubocop offences from newly introduced cops

--- spec/support/query_counter.rb
+++ spec/support/query_counter.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 module QueryCounter
-  def count_queries(&block)
+  def count_queries(&)
     query_count = 0
     ActiveSupport::Notifications.subscribed(
       lambda { |_name, _start, _finish, _id, payload|
         query_count += 1 unless payload[:name].in?(%w[SCHEMA TRANSACTION])
       },
       'sql.active_record',
-      &block
+      &
     )
     query_count
   end
