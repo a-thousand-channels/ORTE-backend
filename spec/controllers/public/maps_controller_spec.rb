a89254ad1432d9108983199a9870f7936386f497
Birte Goldt <bigo@akra.de>
2025-03-07 15:25:50 +0100

    feat: #368 table filtering with match_all param for public maps#show request

--- spec/controllers/public/maps_controller_spec.rb
+++ spec/controllers/public/maps_controller_spec.rb
@@ -76,21 +76,37 @@ RSpec.describe Public::MapsController, type: :controller do
         expect(json['map']['layer'][0]['places'].length).to eq 3
       end
 
-      it 'returns only accordingly tagged places when filtered by tag' do
-        map = Map.create! valid_attributes
-        layer = FactoryBot.create(:layer, map_id: map.id, published: true)
-        place1 = FactoryBot.create(:place, layer: layer, published: true, tag_list: %w[aaa bbb ccc])
-        place2 = FactoryBot.create(:place, layer: layer, published: true, tag_list: %w[ddd eee])
-        place3 = FactoryBot.create(:place, layer: layer, published: true, tag_list: %w[fff])
+      context 'when filtering by tags' do
+        let!(:map) { create(:map, valid_attributes) }
+        let!(:layer) { create(:layer, map_id: map.id, published: true) }
+        let!(:place1) { create(:place, layer: layer, published: true, tag_list: %w[aaa bbb ccc]) }
+        let!(:place2) { create(:place, layer: layer, published: true, tag_list: %w[ddd eee]) }
+        let!(:place3) { create(:place, layer: layer, published: true, tag_list: %w[bbb fff]) }
+        let!(:place4) { create(:place, layer: layer, published: true, tag_list: %w[fff aaa bbb]) }
 
-        get :show, params: { id: map.friendly_id, filter_by_tags: 'bbb,fff', format: 'json' }, session: valid_session
-        expect(response).to have_http_status(200)
-        json = JSON.parse(response.body)
-        expect(json['map']['layer'][0]['places'].length).to eq 2
-        expect(json['map']['layer'][0]['places'][0]['title']).to eq place1.title
-        expect(json['map']['layer'][0]['places'][1]['title']).to eq place3.title
-        expect(json['map']['layer'][0]['places'][0]['tags']).to eq %w[aaa bbb ccc]
-        expect(json['map']['layer'][0]['places'][1]['tags']).to eq %w[fff]
+        it 'returns only accordingly tagged places with any tag when filtered by tag' do
+          get :show, params: { id: map.friendly_id, filter_by_tags: 'bbb,fff', format: 'json' }, session: valid_session
+          expect(response).to have_http_status(200)
+          json = JSON.parse(response.body)
+          expect(json['map']['layer'][0]['places'].length).to eq 3
+          expect(json['map']['layer'][0]['places'][0]['title']).to eq place1.title
+          expect(json['map']['layer'][0]['places'][1]['title']).to eq place3.title
+          expect(json['map']['layer'][0]['places'][2]['title']).to eq place4.title
+          expect(json['map']['layer'][0]['places'][0]['tags']).to eq %w[aaa bbb ccc]
+          expect(json['map']['layer'][0]['places'][1]['tags']).to eq %w[bbb fff]
+          expect(json['map']['layer'][0]['places'][2]['tags']).to eq %w[aaa bbb fff]
+        end
+
+        it 'returns only accordingly tagged places with all tags when filtered by tag with match all param' do
+          get :show, params: { id: map.friendly_id, filter_by_tags: 'bbb,fff', match_all: true, format: 'json' }, session: valid_session
+          expect(response).to have_http_status(200)
+          json = JSON.parse(response.body)
+          expect(json['map']['layer'][0]['places'].length).to eq 2
+          expect(json['map']['layer'][0]['places'][0]['title']).to eq place3.title
+          expect(json['map']['layer'][0]['places'][1]['title']).to eq place4.title
+          expect(json['map']['layer'][0]['places'][0]['tags']).to eq %w[bbb fff]
+          expect(json['map']['layer'][0]['places'][1]['tags']).to eq %w[aaa bbb fff]
+        end
       end
 
       it 'can handle images with and without sorting values' do
