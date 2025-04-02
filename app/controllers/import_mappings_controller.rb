73d25760446c7a8d3eb22a01803d515621f010e4
Birte Goldt <bigo@akra.de>
2025-04-02 16:35:12 +0200

    feat: add drop-down to select existed mappings, validate fields on import mapping creation and redirect to new action in case creation fails #400

--- app/controllers/import_mappings_controller.rb
+++ app/controllers/import_mappings_controller.rb
@@ -14,15 +14,27 @@ class ImportMappingsController < ApplicationController
     @headers = params[:headers]
     @place_columns = Place.column_names
     @import_mapping = ImportMapping.new
+    @existing_mappings = ImportMapping.all.select do |mapping|
+      JSON.parse(mapping.mapping).all? { |m| @headers.include?(m['csv_column_name']) }
+    end
   end
 
   def create
     @import_mapping = ImportMapping.new(import_mapping_params)
+    @headers = JSON.parse(params[:import_mapping][:headers])
+    @place_columns = Place.column_names
+    @existing_mappings = ImportMapping.all.select do |mapping|
+      JSON.parse(mapping.mapping).all? { |m| @headers.include?(m['csv_column_name']) }
+    end
 
-    if @import_mapping.save
-      redirect_to @import_mapping
-    else
-      render :new
+    respond_to do |format|
+      if @import_mapping.save
+        format.html { redirect_to @import_mapping, notice: 'Import mapping was successfully created.' }
+        format.json { render :show, status: :created, location: @import_mapping }
+      else
+        format.html { render :new, missing_fields: @import_mapping.errors[:mapping], headers: @headers }
+        format.json { render json: @import_mapping.errors, status: :unprocessable_entity }
+      end
     end
   end
 
--- app/models/import_mapping.rb
+++ app/models/import_mapping.rb
@@ -1,5 +1,19 @@
 # frozen_string_literal: true
 
 class ImportMapping < ApplicationRecord
+  validate :validate_required_model_properties
+  validates :name, presence: true
+  validates :name, uniqueness: true
 
+  private
+
+  def validate_required_model_properties
+    required_properties = %w[title lat lon]
+    mappings = JSON.parse(mapping || '[]')
+    missing_properties = required_properties - mappings.map { |m| m['model_property'] }
+
+    if missing_properties.any?
+      errors.add(:mapping, "is missing required properties: #{missing_properties.join(', ')}")
+    end
+  end
 end
