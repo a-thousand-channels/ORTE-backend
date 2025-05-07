95453e5bbfdb6790157941c715afe39cfbecc8bc
Ulf Treger <ulf.treger@3plusx.de>
2025-05-07 18:29:41 +0200

    Fix two UI/CSS issues

--- app/assets/javascripts/helpers/info_wrapper.js
+++ app/assets/javascripts/helpers/info_wrapper.js
@@ -2,11 +2,11 @@ $(document).foundation();
 $(document).ready(function() {
   const savedState = localStorage.getItem('info-wrapper-state');
   if (savedState === 'expanded') {
-    $('#info-wrapper-content').addClass('expanded');
+    $('#filter-wrapper-content').addClass('expanded');
     $('#info-wrapper-button').attr('aria-expanded', 'true');
   };            
   $(document).on('click', 'button[data-toggle]', function() {
-    if ($('#info-wrapper-content').hasClass('expanded')) {
+    if ($('#filter-wrapper-content').hasClass('expanded')) {
       localStorage.setItem('info-wrapper-state', 'closed');
     } else {
       localStorage.setItem('info-wrapper-state', 'expanded');
