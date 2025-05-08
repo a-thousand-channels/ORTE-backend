e3388e5dddd081437bee8913e9f5a4575aff9571
Birte Goldt <bigo@akra.de>
2025-05-08 14:01:27 +0200

    feat: #400 differenciate between valid and invalid duplicates, store import data in rails cache instead of session

--- app/helpers/import_context_helper.rb
+++ app/helpers/import_context_helper.rb
@@ -8,4 +8,24 @@ module ImportContextHelper
   def self.read_tempfile_path(file_name)
     Rails.cache.read("import_context/#{file_name}")[:file_path]
   end
+
+  def self.write_importing_rows(file_name, rows)
+    Rails.cache.write("import_context/#{file_name}/importing_rows", { rows: rows }, expires_in: 1.week)
+  end
+
+  def self.read_importing_rows(file_name)
+    Rails.cache.read("import_context/#{file_name}/importing_rows")[:rows]
+  end
+
+  def self.write_importing_duplicate_rows(file_name, rows)
+    Rails.cache.write("import_context/#{file_name}/importing_duplicate_rows", { rows: rows }, expires_in: 1.week)
+  end
+
+  def self.read_importing_duplicate_rows(file_name)
+    Rails.cache.read("import_context/#{file_name}/importing_duplicate_rows")[:rows]
+  end
+
+  def self.delete_tempfile_path(file_name)
+    Rails.cache.delete("import_context/#{file_name}")
+  end
 end
