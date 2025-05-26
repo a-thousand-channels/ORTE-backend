265d0804c06b7e03203053a27c6e67de84892ac0
Birte Goldt <bigo@akra.de>
2025-05-26 13:59:57 +0200

    feat: #400 use MariaDB for docker container, adjust database config accordingly, re-run migration and overwrite getter and setter for mapping in ImportMapping model in order to handle json objects stored as strings properly

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_03_26_143647) do
-
-  create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
+ActiveRecord::Schema[7.2].define(version: 2025_03_26_143647) do
+  create_table "active_storage_attachments", charset: "utf8mb3", collation: "utf8mb3_general_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
     t.bigint "record_id", null: false
@@ -120,14 +119,15 @@ ActiveRecord::Schema.define(version: 2025_03_26_143647) do
     t.index ["place_id"], name: "index_images_on_place_id"
   end
 
-  create_table "import_mappings", charset: "utf8mb3", force: :cascade do |t|
+  create_table "import_mappings", charset: "utf8mb4", collation: "utf8mb4_uca1400_ai_ci", force: :cascade do |t|
     t.string "name"
-    t.json "mapping"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.text "mapping", size: :long, collation: "utf8mb4_bin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.check_constraint "json_valid(`mapping`)", name: "mapping"
   end
 
-  create_table "layers", charset: "utf8mb3", force: :cascade do |t|
+  create_table "layers", charset: "utf8mb3", collation: "utf8mb3_general_ci", force: :cascade do |t|
     t.string "title"
     t.string "subtitle"
     t.boolean "published", default: false
