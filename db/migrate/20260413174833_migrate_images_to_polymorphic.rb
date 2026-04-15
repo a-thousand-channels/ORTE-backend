f2b0107513339ce3b60a46df25adc4c04a895d6f
Ulf Treger <ulf.treger@3plusx.de>
2026-04-15 13:37:03 +0200

    One fix, make migration more verbose

--- db/migrate/20260413174833_migrate_images_to_polymorphic.rb
+++ db/migrate/20260413174833_migrate_images_to_polymorphic.rb
@@ -1,11 +1,13 @@
 class MigrateImagesToPolymorphic < ActiveRecord::Migration[7.2]
   def up
     Image.where.not(place_id: nil).find_each do |image|
+      puts "Migrating Image ##{image.id} #{image.title} to polymorphic association with Place ##{image.place_id}"
       image.update!(
         # avoid strip_exif_data is getting called:
         skip_beforesave_callback: true,
         imageable_type: 'Place',
-        imageable_id: image.place_id
+        imageable_id: image.place_id,
+        title: image.title.presence || "Image for Place ##{image.place_id}"
       )
     end
   end
