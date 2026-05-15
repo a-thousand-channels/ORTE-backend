6c657202f3406e68608ae9c1c2ba789c9c6918e4
Ulf Treger <ulf.treger@3plusx.de>
2026-05-15 13:59:55 +0200

    Fix SMTP config/2

--- config/environment.rb
+++ config/environment.rb
@@ -11,7 +11,7 @@ ActionMailer::Base.smtp_settings = {
       address: Rails.application.credentials.dig(:mail, :address),
       domain: Rails.application.credentials.dig(:mail, :domain),
       port: '465',
-      tsl: true,
+      ssl: true,
       authentication: :login,
       user_name: Rails.application.credentials.dig(:mail, :user_name),
       password:  Rails.application.credentials.dig(:mail, :password)
@@ -19,4 +19,3 @@ ActionMailer::Base.smtp_settings = {
 
 
 
-      
