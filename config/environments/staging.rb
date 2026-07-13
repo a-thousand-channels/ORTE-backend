e95f0441a263b6d548042c3eb8b50616ad4a7086
Ulf Treger <ulf.treger@3plusx.de>
2026-07-13 13:16:40 +0200

    Set I18n defaults

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -79,7 +79,8 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = [:en, :de]
+  config.i18n.default_locale = :en
+  config.i18n.fallbacks = [:de, :en]
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
