a3f36e23a65b07bfe807743afffe5a0776f16cbc
Ulf Treger <ulf.treger@3plusx.de>
2026-08-12 13:06:54 +0200

    Fix

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,6 +1,6 @@
 if Rails.env.production? || Rails.env.staging?
   DEPLOY_REVISION =  File.read(File.join(Rails.root, 'REVISION'))[0..7]
-  DEPLOY_DATE = File.new(File.join(Rails.root, 'REVISION')).ctime.strftime('%d.%m.%Y').to_s
+  DEPLOY_DATE = File.new(File.join(Rails.root, 'REVISION')).ctime
 else
   DEPLOY_REVISION = ""
   DEPLOY_DATE = DateTime.now
