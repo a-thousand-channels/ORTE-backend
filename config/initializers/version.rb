eb736ecb3daa266663a1b703906889be4c5b78ea
Ulf Treger <ulf.treger@3plusx.de>
2026-08-12 12:43:45 +0200

    Renable revision date

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,7 +1,7 @@
-# if Rails.env.production? || Rails.env.staging?
-#  DEPLOY_REVISION =  File.read(File.join(Rails.root, 'REVISION'))[0..7]
-#  DEPLOY_DATE = File.new(File.join(Rails.root, 'REVISION')).ctime
-# else
+if Rails.env.production? || Rails.env.staging?
+  DEPLOY_REVISION =  File.read(File.join(Rails.root, 'REVISION'))[0..7]
+  DEPLOY_DATE = File.new(File.join(Rails.root, 'REVISION')).ctime.strftime('%d.%m.%Y').to_s
+else
   DEPLOY_REVISION = ""
   DEPLOY_DATE = DateTime.now
-# end
+end
