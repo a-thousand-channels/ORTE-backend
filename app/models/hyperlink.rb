9eff4fbfc4dc852a1f8c8da064bd66c125031521
Ulf Treger <ulf.treger@3plusx.de>
2026-08-18 15:31:40 +0200

    Rubocop'd

--- app/models/hyperlink.rb
+++ app/models/hyperlink.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Hyperlink < ApplicationRecord
   belongs_to :place
 
--- spec/factories/hyperlinks.rb
+++ spec/factories/hyperlinks.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :hyperlink do
-    url { "MyString" }
-    linktext { "MyString" }
-    note { "MyText" }
+    url { 'MyString' }
+    linktext { 'MyString' }
+    note { 'MyText' }
   end
 end
